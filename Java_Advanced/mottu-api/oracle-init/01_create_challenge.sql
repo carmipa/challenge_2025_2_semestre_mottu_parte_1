dd8872e4929db0d0c8850100ec7d04dec48d1d90
carmipa <rm557881@fiap.com.br>
2025-08-19 07:54:13 -0300

    Normalize line endings (LF/CRLF fix)

--- Java_Advanced/mottu-api/oracle-init/01_create_challenge.sql
+++
@@ -1,6 +0,0 @@
--- Executado como SYS pelo entrypoint
-ALTER SESSION SET CONTAINER = challenge;  -- se usar XE com XEPDB1, troque aqui
-
-CREATE USER challenge IDENTIFIED BY "2937" ACCOUNT UNLOCK;
-GRANT CONNECT, RESOURCE TO challenge;
-GRANT UNLIMITED TABLESPACE TO challenge;
